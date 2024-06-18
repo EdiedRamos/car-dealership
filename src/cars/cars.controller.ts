483474e5244ba5957b2503f6558dd0685583db21
EdiedRamos <edieisa22@gmail.com>
2024-06-18 02:41:54 -0500

    feat: add car dto validation and controller pipe

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -7,6 +7,8 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import type { Car } from './interfaces/car.interface';
@@ -27,6 +29,7 @@ export class CarsController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createCar(@Body() carBody: CreateCarDto) {
     return {
       method: 'POST',
--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,3 +1,6 @@
+import { IsString } from 'class-validator';
+
 export class CreateCarDto {
+  @IsString()
   readonly brand: string;
 }
